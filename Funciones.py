f81f1ec5f9fd7b06254dc24946212f7bdab0ab90
FabianReyes02 <fa.reyeso@duocuc.cl>
2024-06-17 11:47:08 -0400

    while de cargo

--- Funciones.py
+++ Funciones.py
@@ -6,10 +6,22 @@ def registrarTrabajador():
     listanombre.append(nombre)
     apellido=input("Ingrese su apellido: ")
     listaApellido.append(apellido)
-    cargo=input("Ingrese su cargo: ")
-    listaCargo.append(cargo)
+    while True:
+        cargo=input("Ingrese su cargo\n1.-CEO\n2.-Desarrollador\n3.-Analista de datos\n: ")
+        if cargo==1:
+            cargo="CEO"
+            listaCargo.append(cargo)
+            break
+        elif cargo==2:
+            cargo="Desarrollador"
+            listaCargo.append(cargo)
+            break
+        elif cargo==3:
+            cargo="Analista de datos"
+            listaCargo.append(cargo)
+            break
+        else:
+            print("Dato no valido")
 def listaTodosLosTrabajadores():
     
 def planillaSueldo():
-
-def salirPrograma():
