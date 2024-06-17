0ec5e92907a26eafa96b57535cc6d795b3baff0d
FabianReyes02 <fa.reyeso@duocuc.cl>
2024-06-17 11:26:02 -0400

    registrar trabajador

--- Funciones.py
+++ Funciones.py
@@ -1,8 +1,12 @@
+listanombre=[]
+listaApellido=[]
 def registrarTrabajador():
-    
+    nombre=input("Ingrese su nombre: ")
+    listanombre.append(nombre)
+    apellido=input("Ingrese su apellido: ")
+    listaApellido.append(apellido)
 def listaTodosLosTrabajadores():
 
 def planillaSueldo():
 
 def salirPrograma():
-    
