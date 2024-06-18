3400fc3ef1eaf30bce0bd3b98b905438a5dac1e5
FabianReyes02 <fa.reyeso@duocuc.cl>
2024-06-17 21:07:22 -0400

    3.0

--- Funciones.py
+++ Funciones.py
@@ -1,31 +1,37 @@
 listanombre=[]
 listaApellido=[]
 listaCargo=[]
+cont=0
 def registrarTrabajador():
     nombre=input("Ingrese su nombre: ")
     listanombre.append(nombre)
     apellido=input("Ingrese su apellido: ")
     listaApellido.append(apellido)
+    cont=1+cont
     while True:
-        cargo=int(input("Ingrese su cargo\n1.-CEO\n2.-Desarrollador\n3.-Analista de datos\n: "))
-        if cargo==1:
-            cargo="CEO"
-            listaCargo.append(cargo)
-            break
-        elif cargo==2:
-            cargo="Desarrollador"
-            listaCargo.append(cargo)
-            break
-        elif cargo==3:
-            cargo="Analista de datos"
-            listaCargo.append(cargo)
-            break
+        try:
+            cargo=int(input("Ingrese su cargo\n1.-CEO\n2.-Desarrollador\n3.-Analista de datos\n: "))
+        except:
+            print("Opción invalida")
         else:
-            print("Dato no valido")
+            if cargo==1:
+                cargo="CEO"
+                listaCargo.append(cargo)
+                break
+            elif cargo==2:
+                cargo="Desarrollador"
+                listaCargo.append(cargo)
+                break
+            elif cargo==3:
+                cargo="Analista de datos"
+                listaCargo.append(cargo)
+                break
+            else:
+                print("Dato no valido")
     archivo=open('Fundamentos-de-Programacion/texto.txt', 'a')
     archivo.write(f"{nombre},{apellido},{cargo}\n")
 def listaTodosLosTrabajadores():
-    for i in range(listanombre):
-        print(listanombre,listaApellido,listaCargo)
+    for i in range(cont):
+        print(f"{listanombre},{listaApellido},{listaCargo}\n")
 def planillaSueldo():
         print("si")
